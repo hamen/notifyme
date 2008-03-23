9b2b63f7e6e38c7f83af8477e98b71fd40afb5c7
imorgillo <imorgillo@sanniolug.org>
2008-03-23 17:32:57 +0100

    Get notified when chosen contact goes online, offline, (partially: busy or away)
    darcs-hash:20080323163257-c3cd8-53bd2ebe73aed0295b5e5f612849f2aad96e39ef.gz

--- content/overlay.js
+++ content/overlay.js
@@ -1,5 +1,3 @@
-var address;
-
 var Dialog = {
   onLoad: function init() {
     // initialization code
@@ -14,13 +12,23 @@ var Dialog = {
 	address = xulContact.getAttribute('address');
 	//alert('address is: ' + address);
 
+
 	// Open Dialog and pass it clicked contact JID address 
 	var params = {inn:{address:address, online:true}, out:null};       
 	window.openDialog("chrome://notifyme/content/dialog.xul", "",
 			  "chrome, dialog, modal, resizable=yes", params).focus();
 	if (params.out) {
 	    // User clicked ok. Process changed arguments; e.g. write them to disk or whatever
-	    alert('Got out params \n online checkbox is: ' + params.out.online + ' and offline is: ' +params.out.offline);
+	    alert('Got out params \n online checkbox is: ' + params.out.online + '\n offline is: ' +params.out.offline);
+	    
+	    // Start to sniff the channel
+	    channel.on({
+		    event : 'presence',
+			direction : 'in',
+			stanza : function(s) {
+			return XMPP.JID(s.@from).address == address;
+		    }},
+		function(presence) { detectedContact(presence, params); });
 	}
 	else {
 	    // User clicked cancel. Typically, nothing is done here.
@@ -28,4 +36,17 @@ var Dialog = {
     }
 };
 
+var address;
+
 window.addEventListener("load", function(e) { Dialog.onLoad(e); }, false);
+
+function detectedContact(presence, params) {
+    if(presence.stanza.@type == 'unavailable' && params.out.offline)
+	alert(address + ' has gone offline!');
+    else if (presence.stanza.show == 'away')
+	alert(address + ' changed status to AWAY');
+    else if (presence.stanza.show == 'dnd')
+	alert(address + ' changed status to BUSY');
+    else if (params.out.online)
+	alert(address + ' has come online!');
+}
