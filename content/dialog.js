4b82adef28f978136e3490c6fdfa7cef958ecf62
imorgillo <imorgillo@sanniolug.org>
2008-03-23 16:38:31 +0100

    I can pass params.inn from Overlay to Dialog and get params.out back
    darcs-hash:20080323153831-c3cd8-91142564058ab9280f105e1e332bbfdf83b2e9ee.gz

---
+++ content/dialog.js
@@ -0,0 +1,22 @@
+// Called once when the dialog displays
+function onLoad() {
+  // Use the arguments passed to us by the caller
+  document.getElementById("address").value = window.arguments[0].inn.address;
+  document.getElementById("online").checked = window.arguments[0].inn.online;
+}
+
+// Called once if and only if the user clicks OK
+function onOK() {
+   // Return the changed arguments.
+   // Notice if user clicks cancel, window.arguments[0].out remains null
+   // because this function is never called
+   window.arguments[0].out = {address:document.getElementById("address").value,
+			      online:document.getElementById("online").checked,
+			      offline:document.getElementById("offline").checked};
+   return true;
+}
+
+function onCancel(){
+
+    alert('Cancel');
+}
--- content/overlay.js
+++ content/overlay.js
@@ -1,13 +1,31 @@
+var address;
+
 var Dialog = {
-  onLoad: function() {
+  onLoad: function init() {
     // initialization code
     this.initialized = true;
     var channel = XMPP.createChannel();
+
   },
 
-  onMenuItemCommand: function() {
-    window.open("chrome://notifyme/content/dialog.xul", "", "chrome");
-  }
+  notifyMe: function showDialog(xulPopupNode) {
+	// Get clicked contact JID address
+	var xulContact = $(xulPopupNode, '^ .contact');
+	address = xulContact.getAttribute('address');
+	//alert('address is: ' + address);
+
+	// Open Dialog and pass it clicked contact JID address 
+	var params = {inn:{address:address, online:true}, out:null};       
+	window.openDialog("chrome://notifyme/content/dialog.xul", "",
+			  "chrome, dialog, modal, resizable=yes", params).focus();
+	if (params.out) {
+	    // User clicked ok. Process changed arguments; e.g. write them to disk or whatever
+	    alert('Got out params \n online checkbox is: ' + params.out.online + ' and offline is: ' +params.out.offline);
+	}
+	else {
+	    // User clicked cancel. Typically, nothing is done here.
+	}
+    }
 };
 
 window.addEventListener("load", function(e) { Dialog.onLoad(e); }, false);
