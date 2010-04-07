7f13c7a8c5214fb370fe19e375a82e7a75d349fc
Ivan Morgillo <imorgillo@sanniolug.org>
2010-04-07 13:06:29 +0200

    getElementsByClass draft

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -223,3 +223,21 @@ sameplacepp.User = function( a, n, b, c){
     this.boxes = b;
     this.counts = c;
 };
+
+sameplacepp.getElementsByClass = function(searchClass,node,tag) {
+	var classElements = new Array();
+	if ( node == null )
+		node = document;
+	if ( tag == null )
+		tag = '*';
+	var els = node.getElementsByTagName(tag);
+	var elsLen = els.length;
+	var pattern = new RegExp('(^|\\\\s)'+searchClass+'(\\\\s|$)');
+	for (i = 0, j = 0; i < elsLen; i++) {
+		if ( pattern.test(els[i].className) ) {
+			classElements[j] = els[i];
+			j++;
+		}
+	}
+	return classElements;
+};
